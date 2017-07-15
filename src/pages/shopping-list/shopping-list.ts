7d4adcf54d701948ff48075f0703636339120a37
daniel ra <winra001@hotmail.com>
2017-07-15 21:03:06 +1000

    111. Submitting the Form (Template-Driven)

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ShoppingListPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'page-shopping-list',
@@ -14,11 +7,8 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ShoppingListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ShoppingListPage');
+  onAddItem(form: NgForm) {
+    console.log(form);
   }
 
 }
