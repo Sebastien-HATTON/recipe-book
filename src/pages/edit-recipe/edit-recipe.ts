202ea68350fc7cb8299b305c045722239b819296
daniel ra <winra001@hotmail.com>
2017-07-19 23:21:09 +1000

    137. Updating the Recipe through a Service

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -43,7 +43,12 @@ export class EditRecipePage implements OnInit {
       });
     }
 
-    this.recipesService.addRecipe(value.title, value.description, value.difficulty, ingredients);
+    if (this.mode == 'Edit') {
+      this.recipesService.updateRecipe(this.index, value.title, value.description, value.difficulty, ingredients);
+    } else {
+      this.recipesService.addRecipe(value.title, value.description, value.difficulty, ingredients);
+    }
+
     this.recipeForm.reset();
     this.navCtrl.popToRoot();
   }
