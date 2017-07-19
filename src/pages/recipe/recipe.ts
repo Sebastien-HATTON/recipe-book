c5632ff3117d1adbc4c372ed49575adf465d5e2d
daniel ra <winra001@hotmail.com>
2017-07-19 23:41:50 +1000

    138. Sending Ingredients to the Shopping List

--- src/pages/recipe/recipe.ts
+++ src/pages/recipe/recipe.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Recipe } from "../../models/recipe";
 import { EditRecipePage } from "../edit-recipe/edit-recipe";
+import { ShoppingListService } from "../../services/shopping-list-service";
+import { RecipesService } from "../../services/recipes-service";
 
 @Component({
   selector: 'page-recipe',
@@ -12,7 +14,10 @@ export class RecipePage implements OnInit {
   recipe: Recipe;
   index: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private shoppingListService: ShoppingListService,
+              private recipesService: RecipesService) {
   }
 
   ngOnInit() {
@@ -22,6 +27,15 @@ export class RecipePage implements OnInit {
 
   onEditRecipe() {
     this.navCtrl.push(EditRecipePage, {mode: 'Edit', recipe: this.recipe, index: this.index});
+  }
+
+  onAddIngredients() {
+    this.shoppingListService.addItems(this.recipe.ingredients);
+  }
+
+  onDeleteRecipe() {
+    this.recipesService.removeRecipe(this.index);
+    this.navCtrl.popToRoot();
   }
 
 }
