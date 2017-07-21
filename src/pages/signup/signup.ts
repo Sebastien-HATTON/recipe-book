aebca1b0e6e069f945159b2f7c39c9f2ada3426b
daniel ra <winra001@hotmail.com>
2017-07-21 20:57:28 +1000

    151. Implementing the Signup Process

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import { AuthService } from "../../services/auth-service";
 
 @Component({
   selector: 'page-signup',
@@ -7,8 +8,13 @@ import { NgForm } from "@angular/forms";
 })
 export class SignupPage {
 
+  constructor(private authService: AuthService) {
+  }
+
   onSignup(form: NgForm) {
-    console.log(form.value);
+    this.authService.signup(form.value.email, form.value.password)
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
   }
 
 }
