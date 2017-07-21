6155ccb37d198cb983770ffa1bb93114e34e5a0a
daniel ra <winra001@hotmail.com>
2017-07-21 20:43:18 +1000

    151. Implementing the Signup Process

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component, ViewChild } from '@angular/core';
 import { MenuController, NavController, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+// Install Firebase SDK
+// $npm install --save firebase
+import firebase from 'firebase';
+
 import { TabsPage } from "../pages/tabs/tabs";
 import { SigninPage } from "../pages/signin/signin";
 import { SignupPage } from "../pages/signup/signup";
@@ -17,6 +22,11 @@ export class MyApp {
   @ViewChild('nav') nav: NavController;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDUy_9c7jGJkI7ZmgO4y49Q7ljzEqdvbB8",
+      authDomain: "ionic-recipebook-d6a0c.firebaseapp.com"
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
