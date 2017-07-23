4e783193182bab3e154936fc39d67ae8c60c5866
daniel ra <winra001@hotmail.com>
2017-07-23 14:55:06 +1000

    146. Adding a Sidemenu

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { ShoppingListService } from "../../services/shopping-list-service";
 import { Ingredient } from "../../models/ingredient";
-import { PopoverController } from "ionic-angular";
+import { AlertController, LoadingController, PopoverController } from "ionic-angular";
 import { ShoppingListOptionsPage } from "./shopping-list-options/shopping-list-options";
 import { AuthService } from "../../services/auth-service";
 
@@ -16,7 +16,9 @@ export class ShoppingListPage {
 
   constructor(private shoppingListService: ShoppingListService,
               private popoverCtrl: PopoverController,
-              private authService: AuthService) {
+              private authService: AuthService,
+              private loadingCtrl: LoadingController,
+              private alertCtrl: AlertController) {
   }
 
   ionViewWillEnter() {
@@ -35,17 +37,23 @@ export class ShoppingListPage {
   }
 
   onShowOptions(event: MouseEvent) {
+    const loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+
     const popover = this.popoverCtrl.create(ShoppingListOptionsPage);
     popover.present({ev: event});
     popover.onDidDismiss(
       data => {
         if (data.action == 'load') {
+          loading.present();
           this.authService.getActiveUser().getToken()
             .then(
               (token: string) => {
                 this.shoppingListService.fetchList(token)
                   .subscribe(
                     (list: Ingredient[]) => {
+                      loading.dismiss();
                       if (list) {
                         this.listItems = list;
                       } else {
@@ -53,20 +61,23 @@ export class ShoppingListPage {
                       }
                     },
                     error => {
-                      console.log(error);
+                      loading.dismiss();
+                      this.handleError(error.message);
                     }
                   );
               }
             );
-        } else  {
+        } else if (data.action == 'store') {
+          loading.present();
           this.authService.getActiveUser().getToken()
             .then(
               (token: string) => {
                 this.shoppingListService.storeList(token)
                   .subscribe(
-                    () => console.log('Success!'),
+                    () => loading.dismiss(),
                     error => {
-                      console.log(error);
+                      loading.dismiss();
+                      this.handleError(error.message);
                     }
                   );
               }
@@ -78,6 +89,15 @@ export class ShoppingListPage {
 
   private loadItems() {
     this.listItems = this.shoppingListService.getItems();
+  }
+
+  private handleError(errorMessage: string) {
+    const alert = this.alertCtrl.create({
+      title: 'An error occurred!',
+      message: errorMessage,
+      buttons: ['Ok']
+    });
+    alert.present();
   }
 
 }
